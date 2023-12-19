8dc3e16861f9bcf9a21c5e5c858c565b62bc4510
AndresAnchundia <136878457+AndresAnchundia@users.noreply.github.com>
2023-12-19 02:20:11 -0500

    cuarto commit

--- app.py
+++ app.py
@@ -10,20 +10,20 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 app = Flask(__name__)
 
-#app.config["SQLALCHEMY_DATEBASE_URI"]="sqlite:///pokedex.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///pokedex.sqlite"
 
-#db=SQLAlchemy(app)
+db=SQLAlchemy(app)
 
-#class Pokemon(db.Model):
-#    id: Mapped[int] = mapped_column(db.Integer,primary_key=True,autoincrement=True)
-#    name: Mapped[str] = mapped_column(db.String,nullable=False)
-#    height: Mapped[float] = mapped_column(db.Float,nullable=False)
-#    weight: Mapped[float] = mapped_column(db.Float,nullable=False)
-#    order: Mapped[int] = mapped_column(db.Integer,nullable=False)
-#    type: Mapped[str] = mapped_column(db.String,nullable=False)
+class Pokemon(db.Model):
+    id: Mapped[int] = mapped_column(db.Integer,primary_key=True,autoincrement=True)
+    name: Mapped[str] = mapped_column(db.String,nullable=False)
+    height: Mapped[float] = mapped_column(db.Float,nullable=False)
+    weight: Mapped[float] = mapped_column(db.Float,nullable=False)
+    order: Mapped[int] = mapped_column(db.Integer,nullable=False)
+    type: Mapped[str] = mapped_column(db.String,nullable=False)
 
-#with app.app_context():
-#    db.create_all()
+with app.app_context():
+    db.create_all()
     
 def get_pokemon_data(pokemon):
     url=f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
@@ -36,11 +36,11 @@ def home():
     if request.method == 'POST':
         name_pokemon = request.form.get('name')
         if name_pokemon: 
-            data = get_pokemon_data(name_pokemon.lower())
+            data = get_pokemon_data(name_pokemon.lower())#el nombre del pokemon se lee en minuscula.
             if data:
                 pokemon={'id':data.get('id'), 
-                        'name':data.get('name').upper(),
-                        'height': data.get('height'), #con esta sentencia buscamos el json
+                        'name':data.get('name').upper(),#con esta sentencia buscamos el json
+                        'height': data.get('height'), 
                         'weight':data.get('weight'),
                         'order':data.get('order'),
                         'type':'Profesor',
@@ -48,23 +48,54 @@ def home():
                         }
     return render_template('pokemon.html', pokemon=pokemon)
 
-@app.route("/home1")
-def home1():
-    return render_template('detalle.html')
 
+#@app.route("/detalle/<hp>/<attack>/<defence>/<speed>/")
+#def detalle(hp,attack,defence,speed):
+#    return render_template('detalle.html', hp=hp, attack=attack, defence=defence, speed=speed)
+
+@app.route("/detalle/<id>")
+def detalle(id):
+    data = get_pokemon_data(id)
+    pokemon = {
+        'photo': data.get("sprites").get("other").get("official-artwork").get("front_default"),
+        'name': data.get("name").upper(),
+        'hp': data.get("stats")[0].get("base_stat"),
+        'attack': data.get("stats")[1].get("base_stat"),
+        'defence': data.get("stats")[2].get("base_stat"),
+        'speed': data.get("stats")[5].get("base_stat"),
+    }
+    return render_template("detalle.html", pokemon=pokemon)
  
 #Agregar la información a la base de datos
 #@app.route("/insert_pokemon/<pokemon>")
 #def insert(pokemon):
-#    new_pokemon=pokemon
+#       new_pokemon=pokemon
 #    if new_pokemon:
-#        obj=Pokemon(pokemon)
+#        obj=pokemon  #obj=Pokemon(pokemon) 
 #        db.session.add(obj)
 #        db.session.commit()
-#    return 'Pokemon Agregado'
+#   return 'Pokemon Agregado'
+
+
+#@app.route("/select/<name>")
+#def selectbyname(name):
+#    poke = Pokemon.query.filter_by(name=name).first()
+#    return str(poke.id) + str(poke.name)
+
+#@app.route("/selectbyid/<id>")
+#def selectbyid(id):
+#    poke = Pokemon.query.filter_by(id=id).first()
+#    return str(poke.id) + str(poke.name)
+
+#@app.route("/deletebyid/<id>")
+#def deletebyid(id):
+#    pokemon_a_eliminar = Pokemon.query.filter_by(id=id).first()
+#    db.session.delete(pokemon_a_eliminar)
+#    db.session.commit()
+#    return 'Pokemon Eliminado!'
 
 
 if __name__=='__main__':
     app.run(debug=True)
     
-   
+    
